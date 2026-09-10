73a26388bf98e067230fa0dfa9b953fdcffc1835
ssvcs <ssvcs@rit.edu>
2026-09-10 15:17:13 -0400

    starter code changes from faculty meeting.

--- src/words.py
+++ src/words.py
@@ -9,5 +9,29 @@ import typer
 
 app = typer.Typer(help="CSAPX Project 1: Words - A unified CLI for unigram analysis.")
 
+@app.command()
+def word_count() -> None:
+    return None
+
+@app.command()
+def first_appearance() -> None:
+    return None
+
+@app.command()
+def letter_freq() -> None:
+    return None
+
+@app.command()
+def word_length() -> None:
+    return None
+
+@app.command()
+def word_velocity() -> None:
+    return None
+
+@app.command()
+def word_freq() -> None:
+    return None
+
 if __name__ == "__main__":
     app()
